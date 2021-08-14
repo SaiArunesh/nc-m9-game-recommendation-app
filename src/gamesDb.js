83cd6d4e4cda7ff833818d51700b2d178ca3b43e
Sai Arunesh <vssaiarunesh@gmail.com>
2021-08-15 00:03:30 +0530

    added 1 more recommendation under each category

--- src/gamesDb.js
+++ src/gamesDb.js
@@ -2,31 +2,43 @@ const gamesDb = {
   RPG: [
     {
       title: "Horizon Zero Dawn",
-      description: "Post apocalyptic mecha adventure"
+      description: "5/5 Post apocalyptic mecha adventure"
     },
     {
       title: "Rise of Tomb Raider",
-      description: "Gods are as real as they get"
+      description: "4/5 Gods are as real as they get"
+    },
+    {
+      title: "Prince of Persia",
+      description: "4/5 Just for the nostalgia"
     }
   ],
   FPS: [
     {
       title: "Apex Legend",
-      description: "Who does not play it?"
+      description: "4/5 Who does not play it?"
     },
     {
       title: "COD",
-      description: "Roger that"
+      description: "4/5 Roger that"
+    },
+    {
+      title: "BGMI",
+      description: "4/5 Every Indian plays this"
     }
   ],
   "puzzle-y": [
     {
       title: "Immortals Fenyx Rsing",
-      description: "Where is Medusa?"
+      description: "5/5 Where is Medusa?"
     },
     {
-      title: "Assasins Creed",
-      description: "Non scalable walls are a huge pain"
+      title: "Assasins Creed Valhalla",
+      description: "5/5 Non scalable walls are a huge pain"
+    },
+    {
+      title: "Flow free",
+      description: "5/5 Extremely addictive"
     }
   ]
 };
